67d85fd2a4acecef040ac0a37aeb7c03d73c79c4
Stanislas Selle <stanworld@gmail.com>
2024-11-25 13:10:40 +0100

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<!-- navigation.php -->
+<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="about.php">À propos</a></li>
+        <li><a href="experiments.php">Expériences en cours</a></li>
+        <li><a href="research.php">Veille technologique</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
